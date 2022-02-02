300dcee0ff28091863c6cf7cb9245ca7e9111e12
Mert Kırpıcı <selimmertkirpici@vituscommodities.com>
2022-02-02 14:58:41 +0300

    run: add livereload and detached

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ containername="kirpici-container"
 docker stop ${containername} || true
 
 docker run  \
+           -d \
            --rm \
            -v $(pwd):/srv/jekyll \
            --publish 127.0.0.1:4000:4000 \
            --name ${containername} \
            jekyll/jekyll:4.2.0 \
-           jekyll serve
+           jekyll serve --liverleload
