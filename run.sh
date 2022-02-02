9e3da5381802cedebab5f6b841c81c2183e3d196
Mert Kırpıcı <selimmertkirpici@vituscommodities.com>
2022-02-02 14:55:06 +0300

    run: add

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+containername="kirpici-container"
+
+docker stop ${containername} || true
+
+docker run  \
+           --rm \
+           -v $(pwd):/srv/jekyll \
+           --publish 127.0.0.1:4000:4000 \
+           --name ${containername} \
+           jekyll/jekyll:4.2.0 \
+           jekyll serve
